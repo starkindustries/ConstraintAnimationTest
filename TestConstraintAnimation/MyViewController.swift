9827bf6445df18e07233e9f5ecb398b7dd7dd0f1
Zion Perez <zionperez@gmail.com>
2017-03-30 09:20:34 +0900

    Clean up code.

--- TestConstraintAnimation/MyViewController.swift
+++ TestConstraintAnimation/MyViewController.swift
@@ -10,14 +10,8 @@ import UIKit
 
 class MyViewController: UIViewController {
     
-    // Hello World UITextField and TextField Border vars
-    @IBOutlet weak var helloWorldTextField: UITextField!
-    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    // Hello World TextField Border var
     @IBOutlet weak var borderTextFieldWidth: NSLayoutConstraint!
-    
-    // Hello Label Vars
-    @IBOutlet weak var helloLabelWidthConstraint: NSLayoutConstraint!
-    @IBOutlet weak var helloLabel: UILabel!
     
     // Button Vars
     @IBOutlet weak var myButton: UIButton!
@@ -27,9 +21,6 @@ class MyViewController: UIViewController {
         if grow {
             UIView.animate(withDuration: duration, animations: {
                 self.borderTextFieldWidth.constant = 330
-                // self.widthConstraint.constant = 330
-                // self.helloLabelWidthConstraint.constant = 330
-                // self.helloWorldTextField.textAlignment = NSTextAlignment.center
                 self.view.layoutIfNeeded()
             }, completion: { (finished: Bool) in
                 print("Grow animation complete!")
@@ -37,9 +28,6 @@ class MyViewController: UIViewController {
         } else {
             UIView.animate(withDuration: duration, animations: {
                 self.borderTextFieldWidth.constant = 115
-                // self.widthConstraint.constant = 100
-                // self.helloLabelWidthConstraint.constant = 100
-                // self.helloWorldTextField.textAlignment = NSTextAlignment.center
                 self.view.layoutIfNeeded()
             }, completion: { (finished: Bool) in
                 print("Shrink animation complete!")
@@ -47,32 +35,11 @@ class MyViewController: UIViewController {
         }
     }
     
-    func increaseFontSize(duration: TimeInterval) {
-        // helloLabel.font = UIFont.boldSystemFont(ofSize: 35)
-        // helloLabel.transform = helloLabel.transform.scaledBy(x: 1.0, y: 1.0)
-        UIView.animate(withDuration: duration, animations: {
-            self.helloLabel.transform = self.helloLabel.transform.scaledBy(x: 5.0, y: 5.0)
-        })
-    }
-    
-    func decreaseFontSize(duration: TimeInterval) {
-        // helloLabel.font = UIFont.boldSystemFont(ofSize: 35)
-        // helloLabel.transform = helloLabel.transform.scaledBy(x: 1.0, y: 1.0)
-        UIView.animate(withDuration: duration, animations: {
-            self.helloLabel.transform = self.helloLabel.transform.scaledBy(x: 0.2, y: 0.2)
-        })
-    }
-    
     @IBAction func toggle(){
         let duration: TimeInterval = 1.0
         grow = !grow
-        if grow {
-            myButton.setTitle("Shrink", for: UIControlState.normal)
-            increaseFontSize(duration: duration)
-        } else {
-            myButton.setTitle("Grow", for: UIControlState.normal)
-            decreaseFontSize(duration: duration)
-        }
+        let title = grow ? "Shrink" : "Grow"
+        myButton.setTitle(title, for: UIControlState.normal)
         animateGrowShrinkTextFields(grow: grow, duration: duration)
     }
 }
